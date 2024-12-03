471582fef06da4dad2eae1636402a92250e3aa89
Venterum <fiskoev@mail.ru>
2024-12-04 00:22:25 +0700

    Обновлён алгоритм, исправлены ошибки

--- app.py
+++ app.py
@@ -6,42 +6,55 @@ from PyQt6.QtCore import Qt
 
 
 def latinizator(text):
-    table = {
-        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'Jo',
-        'Ж': 'Ž', 'З': 'Z', 'И': 'I', 'Й': 'J', 'К': 'K', 'Л': 'L', 'М': 'M',
-        'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U',
-        'Ф': 'F', 'Х': 'H', 'Ц': 'C', 'Ч': 'Č', 'Ш': 'Š', 'Щ': 'Šch', 'Ы': 'Y',
-        'Э': 'É', 'Ю': 'Ju', 'Я': 'Ja', 'Ь': "", 'Ъ': '',
-        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'jo',
-        'ж': 'ž', 'з': 'z', 'и': 'i', 'й': 'j', 'к': 'k', 'л': 'l', 'м': 'm',
-        'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
-        'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'č', 'ш': 'š', 'щ': 'šch', 'ы': 'y',
-        'э': 'é', 'ю': 'ju', 'я': 'ja', 'ь': "", 'ъ': ''
+    basic_map = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 
+        'е': 'e', 'ё': 'o', 'ж': 'ž', 'з': 'z', 'и': 'i', 
+        'й': 'j', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 
+        'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 
+        'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'č', 
+        'ш': 'š', 'щ': 'šch', 'ы': 'y', 'э': 'é', 
+        'ю': 'u', 'я': 'a',
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 
+        'Е': 'Je', 'Ё': 'Jo', 'Ж': 'Ž', 'З': 'Z', 'И': 'I', 
+        'Й': 'J', 'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N', 
+        'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 
+        'У': 'U', 'Ф': 'F', 'Х': 'H', 'Ц': 'C', 'Ч': 'Č', 
+        'Ш': 'Š', 'Щ': 'Šch', 'Ы': 'Y', 'Э': 'É', 
+        'Ю': 'Ju', 'Я': 'Ja'
     }
-    soft_vowel = {
-        'е': 'je', 'ё': 'jo',
-        'Е': 'Je', 'Ё': 'Jo'
+    soft_map = {
+        'л': 'ĺ', 'т': 't́', 'д': 'd́', 'н': 'ń', 'п': 'ṕ',
+        'с': 'ś', 'р': 'ŕ', 'з': 'ź', 'м': 'ḿ',
+        'Л': 'Ĺ', 'Т': 'T́', 'Д': 'D́', 'Н': 'Ń', 'П': 'Ṕ',
+        'С': 'Ś', 'Р': 'Ŕ', 'З': 'Ź', 'М': 'Ḿ'
     }
-    vowels = "АЕЁИОУЫЭЮЯаеёиоуыэюя"
-
+    vowels = {'а', 'о', 'у', 'э', 'ы', 'я', 'е', 'ё', 'ю', 'и',
+              'А', 'О', 'У', 'Э', 'Ы', 'Я', 'Е', 'Ё', 'Ю', 'И'}
     result = []
-
-    words = text.split()
-
-    for word in words:
-        word_result = []
-        for i, char in enumerate(word):
-            if i == 0 and char in soft_vowel:
-                word_result.append(soft_vowel[char])
-            elif char in ('Ъ', 'ъ') and i + 1 < len(word) and word[i + 1] in soft_vowel:
-                word_result.append(soft_vowel[word[i + 1]])
-                continue
-            elif char in soft_vowel and i > 0 and word[i - 1] in vowels:
-                word_result.append(soft_vowel[char])
+    
+    for i, char in enumerate(text):
+        if char in {'ь', 'ъ'}:
+            continue
+        
+        prev_char = text[i - 1] if i > 0 else ''
+        next_char = text[i + 1] if i + 1 < len(text) else ''
+        
+        if char in {'е', 'ё', 'ю', 'я', 'Е', 'Ё', 'Ю', 'Я', 'И', 'и'} and (i == 0 or prev_char in {'ь', 'ъ'}):
+            result.append('J' + basic_map[char.lower()] if char.isupper() else 'j' + basic_map[char])
+        
+        elif char in {'е', 'ё', 'ю', 'я', 'Е', 'Ё', 'Ю', 'Я', 'И', 'и'}:
+            if i == 0 or next_char.lower() in vowels or next_char in {'ь', 'ъ'}:
+                result.append('J' + basic_map[char.lower()] if char.isupper() else 'j' + basic_map[char])
             else:
-                word_result.append(table.get(char, char))
-        result.append(''.join(word_result))
-    return ' '.join(result)
+                result.append(basic_map[char])
+        
+        elif char in soft_map and next_char.lower() in {'и', 'е', 'ё', 'ю', 'я', 'ь', 'И', 'Е', 'Ё', 'Ю', 'Я'}:
+            result.append(soft_map[char])
+        
+        else:
+            result.append(basic_map.get(char, char))
+    
+    return ''.join(result)
 
 
 class LatinizatorApp(QMainWindow):
